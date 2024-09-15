f86a5c14421770ea899dea4af5c9d527c9f5b837
Candelo30 <luiscandelo30@gmail.com>
2024-09-15 07:09:22 -0500

    Configurando lo que vamos a utilizar

---
+++ CarxyAppWeb/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      "kingfisher-daisy": {
+        50: "#f9f4ff",
+        100: "#f2e6ff",
+        200: "#e6d2ff",
+        300: "#d3aeff",
+        400: "#b87bff",
+        500: "#9d49ff",
+        600: "#8825f8",
+        700: "#7315db",
+        800: "#6317b2",
+        900: "#52148f",
+        950: "#400080",
+      },
+    },
+  },
+  plugins: [],
+};
