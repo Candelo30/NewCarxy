ceb91832e94180d6ccc87a78d764882ba305d36c
Candelo30 <luiscandelo30@gmail.com>
2024-09-15 21:37:32 -0500

    Almacenamiento de usuario

--- CarxyAppWeb/src/app/auth/login/login.component.ts
+++ CarxyAppWeb/src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router, RouterLink } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
@@ -14,12 +15,21 @@ export class LoginComponent {
   username = '';
   password = '';
 
-  constructor(public userService: AuthService, public router: Router) {}
+  constructor(
+    public userService: AuthService,
+    public router: Router,
+    private cookieService: CookieService
+  ) {}
 
   Login() {
     const user = { username: this.username, password: this.password };
     this.userService.IniciarSesion(user).subscribe((data) => {
-      this.userService.AsignarToken(data.token);
+      if (data.token) {
+        this.userService.AsignarToken(data.token);
+      }
+      if (data.user) {
+        this.cookieService.set('loggedInUser', JSON.stringify(data.user), 7); // Expira en 7 días
+      }
 
       this.router.navigate(['/home']);
     });
