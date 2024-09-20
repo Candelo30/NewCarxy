416edadb31a699941d4c157801790ec508bea0a2
Candelo30 <luiscandelo30@gmail.com>
2024-09-19 23:39:42 -0500

    Serivicio para todas las publicaciones

--- CarxyAppWeb/src/app/core/services/publications.service.ts
+++ CarxyAppWeb/src/app/core/services/publications.service.ts
@@ -17,12 +17,16 @@ export class PublicationsService {
   DataPublications(): Observable<any> {
     return this.http.get('http://127.0.0.1:8000/api/publicaciones/');
   }
+  dataAllPublications(): Observable<any> {
+    return this.http.get('http://127.0.0.1:8000/api/allPublication/');
+  }
 
   // Método para esperar que todos los datos sean cargados
   loadAllResources(): Observable<any> {
     // Si tienes otras peticiones, agrégalas a `forkJoin`
     return forkJoin({
       userDataPublications: this.DataPublications(),
+      userDataAllPublications: this.dataAllPublications(),
       // Si tienes más peticiones, puedes agregarlas aquí:
       // otherData: this.http.get('otra-ruta-de-datos'),
     });
