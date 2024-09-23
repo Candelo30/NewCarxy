16749f004d1f11de94830f18e361501acadcbf0d
Candelo30 <luiscandelo30@gmail.com>
2024-09-22 20:36:54 -0500

    Cambios en los guardianes

--- CarxyAppWeb/src/app/auth/auth.guard.ts
+++ CarxyAppWeb/src/app/auth/auth.guard.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
 import { UserService } from '../core/services/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private userData: UserService) {}
+  constructor(
+    private router: Router,
+    private cookieService: CookieService,
+    private userData: UserService
+  ) {}
 
   canActivate(): Observable<boolean> {
-    return this.userData.DataUser().pipe(
-      map((data) => {
-        // Si el usuario ya está autenticado, permitir el acceso a la ruta
-        return true;
-      }),
-      catchError((error) => {
-        // Si hay un error (usuario no autenticado), redirigir al login
-        this.router.navigate(['/login']);
-        return [false]; // Devolver un observable con `false`
-      })
-    );
+    const token = this.cookieService.get('token');
+
+    if (token) {
+      // Si hay un token, verificar el estado del usuario
+      return this.userData.DataUser().pipe(
+        map((data) => {
+          // Si los datos del usuario son válidos, permitir el acceso
+          return true;
+        }),
+        catchError((error) => {
+          // En caso de error, eliminar el token y redirigir al login
+          this.cookieService.delete('token');
+          this.router.navigate(['/login']);
+          return of(false); // Retornar un observable con `false`
+        })
+      );
+    } else {
+      // Si no hay token, redirigir al login
+      this.router.navigate(['/login']);
+      return of(false); // Retornar un observable con `false`
+    }
   }
 }
--- CarxyAppWeb/src/app/auth/no-auth.guard.ts
+++ CarxyAppWeb/src/app/auth/no-auth.guard.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
-import { catchError, map, Observable } from 'rxjs';
-import { UserService } from '../core/services/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NoAuthGuard implements CanActivate {
-  constructor(private router: Router, private userData: UserService) {}
+  constructor(private router: Router, private cookieService: CookieService) {}
 
   canActivate(): Observable<boolean> {
-    return this.userData.DataUser().pipe(
-      map((data) => {
-        this.router.navigate(['/home']);
-        return false;
-      }),
-      catchError((error) => {
-        return [true];
-      })
-    );
+    const token = this.cookieService.get('token');
+
+    if (token) {
+      // Si el usuario ya está autenticado, redirigir a '/home'
+      this.router.navigate(['/home']);
+      return of(false); // No permitir acceso a la ruta
+    } else {
+      // Si no hay token, permitir acceso
+      return of(true);
+    }
   }
 }
