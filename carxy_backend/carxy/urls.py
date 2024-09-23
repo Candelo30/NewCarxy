1e6a71e62f7d62de764fd4fbf86afbf850aff9bb
Candelo30 <luiscandelo30@gmail.com>
2024-09-22 20:35:43 -0500

    Nuevas rutas

--- carxy_backend/carxy/urls.py
+++ carxy_backend/carxy/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for carxy project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -27,4 +10,42 @@ urlpatterns = [
     path("iniciar-sesion/", views.iniciarSesion),
     path("registro/", views.registro),
     path("perfil/", views.PerfilView.as_view(), name="perfil"),
+    # Rutas para Modelos3D
+    path(
+        "modelos/", views.ListaModelos3D.as_view(), name="lista_modelos"
+    ),  # GET y POST
+    path(
+        "modelos/<int:pk>/",
+        views.ModificarEliminarModelo.as_view(),
+        name="modificar_eliminar_modelo",
+    ),  # PUT y DELETE
+    # Ruta para personalizar partes
+    path(
+        "personalizar-parte/<int:pk>/",
+        views.PersonalizarParte.as_view(),
+        name="personalizar_parte",
+    ),  # POST
+    # Rutas para partes
+    path("partes/", views.ListaCrearParte.as_view(), name="lista_crear_parte"),
+    path(
+        "partes/<int:pk>/",
+        views.ModificarEliminarParte.as_view(),
+        name="modificar_eliminar_parte",
+    ),
+    # Rutas para gestionar Personalizaciones
+    path(
+        "personalizaciones/",
+        views.ListaCrearPersonalizacion.as_view(),
+        name="lista_crear_personalizacion",
+    ),  # GET y POST
+    path(
+        "personalizaciones/<int:pk>/",
+        views.ModificarEliminarPersonalizacion.as_view(),
+        name="modificar_eliminar_personalizacion",
+    ),  # PUT y DELETE
+    path(
+        "personalizaciones/<int:pk>/",
+        views.RetrievePersonalizacion.as_view(),
+        name="personalizacion-detalle",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
