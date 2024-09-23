edc16e26375e14ef2e8bbb24af0a44ab7114e91f
Candelo30 <luiscandelo30@gmail.com>
2024-09-22 20:40:30 -0500

    Harcodeando

--- CarxyAppWeb/src/app/admin/admin-routing.module.ts
+++ CarxyAppWeb/src/app/admin/admin-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
 
 const routes: Routes = [
   { path: '', component: AdminDashboardComponent }, // Cambié 'das' a 'dashboard'
   { path: 'dashboard', component: AdminDashboardComponent }, // Cambié 'das' a 'dashboard'
   { path: 'usuarios', component: UsuariosComponent },
+  { path: 'configuracion', component: ConfiguracionComponent },
 ];
 
 @NgModule({
