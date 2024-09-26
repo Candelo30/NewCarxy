76ca7b0f9b63c1904cd9ce3575c0969d2fdc6596
Candelo30 <luiscandelo30@gmail.com>
2024-09-26 15:05:10 -0500

    Modificaciones en el servicio de partes

--- CarxyAppWeb/src/app/core/services/parte.service.ts
+++ CarxyAppWeb/src/app/core/services/parte.service.ts
@@ -6,27 +6,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ParteService {
-  private apiUrl = 'http://localhost:8000/'; // Cambia esto a tu URL de API
+  private apiUrl = 'http://localhost:8000/api'; // Cambia esto a tu URL de API
 
   constructor(private http: HttpClient) {}
 
-  // Obtener todas las partes del usuario autenticado
-  getPartes(): Observable<any> {
-    return this.http.get(`${this.apiUrl}partes/`);
+  // Obtener las partes de un modelo por su ID
+  getPartesPorModelo(modeloId: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/partes/por-modelo/${modeloId}/`);
   }
 
-  // Crear una nueva parte
-  crearParte(parteData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}partes/`, parteData);
-  }
-
-  // Actualizar una parte existente
-  actualizarParte(id: number, parteData: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}partes/${id}/`, parteData);
-  }
-
-  // Eliminar una parte
-  eliminarParte(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}partes/${id}/`);
+  // Guardar partes personalizadas
+  savePartes(partes: any[]): Observable<any> {
+    return this.http.post(`${this.apiUrl}/partes/`, partes);
   }
 }
