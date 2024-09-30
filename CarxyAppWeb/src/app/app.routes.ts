40b49b555b113fb4f59d4ffc6793051096d527f4
Candelo30 <luiscandelo30@gmail.com>
2024-09-30 10:56:41 -0500

    Ruta settings

--- CarxyAppWeb/src/app/app.routes.ts
+++ CarxyAppWeb/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { AuthGuard } from './auth/auth.guard';
 import { ProfileComponent } from './shared/components/profile/profile.component';
 import { NoAuthGuard } from './auth/no-auth.guard';
 import { HelpComponent } from './shared/components/help/help.component';
+import { AdminGuard } from './core/guard/admin.guard';
+import { SettingsComponent } from './shared/components/settings/settings.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [NoAuthGuard] }, // Ruta para el inicio de sesión
@@ -18,7 +20,7 @@ export const routes: Routes = [
     path: 'admin',
     loadChildren: () =>
       import('./admin/admin.module').then((m) => m.AdminModule),
-    canActivate: [AuthGuard], // Solo accesible para usuarios autenticados
+    canActivate: [AuthGuard, AdminGuard], // Solo accesible para usuarios autenticados
   },
   {
     path: 'design',
@@ -29,6 +31,7 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] }, // Acceso restringido
   { path: 'help', component: HelpComponent, canActivate: [AuthGuard] }, // Acceso restringido
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }, // Acceso restringido
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] }, // Acceso restringido
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta predeterminada
   { path: '**', redirectTo: '/home' }, // En caso de rutas no encontradas
 ];
